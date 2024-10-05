827235450d6ee341b272e88a94636ac08f48c918
keshav276 <98476549+keshav276@users.noreply.github.com>
2024-10-06 03:51:05 +0530

    Update drive_sorter_app.py

--- drive_sorter_app.py
+++ drive_sorter_app.py
@@ -22,7 +22,7 @@ def authenticate():
     flow = Flow.from_client_config(
         client_config=CLIENT_CONFIG,
         scopes=SCOPES,
-        redirect_uri=st.secrets["redirect_uri"]
+        redirect_uri=st.secrets["https://gdrive-organizer.streamlit.app/"]
     )
 
     if "token" not in st.session_state:

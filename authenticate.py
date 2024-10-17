1575ea78f47a051c10369a8038b7ee46c299156d
keshav chhabra <98476549+keshhaav@users.noreply.github.com>
2024-10-18 01:36:56 +0530

    Update authenticate.py

--- authenticate.py
+++ authenticate.py
@@ -10,20 +10,32 @@ CLIENT_CONFIG = st.secrets["google_oauth"]
 
 
 def authenticate():
-    flow = Flow.from_client_config(
-        client_config=CLIENT_CONFIG,
-        scopes=SCOPES,
-        redirect_uri="https://gdrive-organizer.streamlit.app/callback"
-    )
+    if "auth_state" not in st.session_state:
+        st.session_state.auth_state = "initial"
 
-    if "token" not in st.session_state:
-        if "code" not in st.experimental_get_query_params():
-            authorization_url, _ = flow.authorization_url(prompt='consent')
-            st.link_button("Click to Authorize", authorization_url)
-            return None
-        else:
+    if st.session_state.auth_state == "initial":
+        flow = Flow.from_client_config(
+            client_config=CLIENT_CONFIG,
+            scopes=SCOPES,
+            redirect_uri="https://gdrive-organizer.streamlit.app/callback"
+        )
+        authorization_url, _ = flow.authorization_url(prompt='consent')
+        st.link_button("Click to Authorize", authorization_url)
+        st.session_state.auth_state = "waiting_for_code"
+        return None
+
+    elif st.session_state.auth_state == "waiting_for_code":
+        if "code" in st.experimental_get_query_params():
+            flow = Flow.from_client_config(
+                client_config=CLIENT_CONFIG,
+                scopes=SCOPES,
+                redirect_uri="https://gdrive-organizer.streamlit.app/callback"
+            )
             flow.fetch_token(code=st.experimental_get_query_params()["code"][0])
             st.session_state.token = flow.credentials.to_json()
+            st.session_state.auth_state = "authenticated"
             st.rerun()
+        return None
 
-    return Credentials.from_authorized_user_info(json.loads(st.session_state.token))
+    elif st.session_state.auth_state == "authenticated":
+        return Credentials.from_authorized_user_info(json.loads(st.session_state.token))
